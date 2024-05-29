0bc05c9d8e8c42c2af9f2a466c6564c85b6c3d7f
Yassine Nassib <nassibyassine@gmail.com>
2024-05-29 21:56:20 +0100

    Folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { APIProvider } from './common/api-provider';
+import { APIProvider } from './api/api-provider';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
